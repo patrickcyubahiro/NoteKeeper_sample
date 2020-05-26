b88421cd31442942bf2ff9c8194d833faea5fa97
patrickcyubahiro <ampatrickcyubahiro@gmail.com>
2020-05-27 00:31:33 +0300

    Initial commit

---
+++ app/src/main/java/com/example/notekeeper/NoteListActivity.java
@@ -0,0 +1,40 @@
+package com.example.notekeeper;
+
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+import android.widget.ListView;
+
+public class NoteListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_note_list);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        initializeDisplayContent();
+        
+    }
+
+    private void initializeDisplayContent() {
+        ListView listNotes = findViewById(R.id.list_notes);
+    }
+
+}
